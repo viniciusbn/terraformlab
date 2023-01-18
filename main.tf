8a359e518d5a0a1ac7627ed9597cce194051df2b
Vinicius Nascimento <3640710+viniciusbn@users.noreply.github.com>
2023-01-18 13:30:36 -0300

    first deployment

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
 provider "aws" {
-  
+  region = "us-east-1"
+}
+resource "aws_instance" "terraformlab1" {
+  count         = 3
+  ami           = "ami-06878d265978313ca"
+  instance_type = "t2.micro"
+  key_name      = "terraformlab1"
+
+  tags = {
+    Name = "terraformlab1-${count.index}"
+  }
 }
